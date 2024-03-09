630762267e10da959b3d18e64541f90aec1ceaab
almasbekkyzyelnura <elnuraalmasbekkyzy8@gmail.com>
2024-03-10 01:01:48 +0600

    тапщшырма 7

---
+++ index.js
@@ -0,0 +1,8 @@
+let total = (15 + 2 +7 ) * 2 ;
+
+alert('Total (15 + 2 + 7) * 2 =' + total);
+
+
+const string = "эМнеГе аЙгаНыш аСельди АЛДАДЫ"
+alert(string [0].toUpperCase() + string[1].toLowerCase() + string[2].toLowerCase() + string[3].toLowerCase() + string[4].toLowerCase() + string[5].toLowerCase() + '  ' +  string[7].toUpperCase() + string[8].toLowerCase()  + string[9].toLowerCase() + string[10].toLowerCase() + string[11].toLowerCase() + string[12].toLowerCase() + string[13].toLowerCase() + '  ' + string [15].toUpperCase() + string[16].toLowerCase() + string[17].toLowerCase()
+ + string[18].toLowerCase() + string[19].toLowerCase() + string[20].toLowerCase() + string[21].toLowerCase() + ' ' + string.slice(22).toLowerCase() + '?');
